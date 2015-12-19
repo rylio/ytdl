3539379b3b19ff07f87fa2de77adfe6671bb534f
Ryan Coffman <ryan.c432@gmail.com>
2015-12-19 06:14:48 -0800

    Bumped to version 0.3.1

--- cmd/ytdl/main.go
+++ cmd/ytdl/main.go
@@ -36,7 +36,7 @@ func main() {
 	app.ArgsUsage = "[youtube url or video id]"
 	app.Usage = "Download youtube videos"
 	app.HideHelp = true
-	app.Version = "0.3.0"
+	app.Version = "0.3.1"
 
 	app.Flags = []cli.Flag{
 		cli.HelpFlag,
